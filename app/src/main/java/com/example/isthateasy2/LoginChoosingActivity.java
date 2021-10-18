77047af57aae93b3aa4d4641ff6758e5349f2406
joxy <josianenyirarukundo3@gmail.com>
2021-10-18 14:37:40 +0200

    small chang

--- app/src/main/java/com/example/isthateasy2/LoginChoosingActivity.java
+++ app/src/main/java/com/example/isthateasy2/LoginChoosingActivity.java
@@ -9,20 +9,34 @@ import android.widget.Button;
 
 public class LoginChoosingActivity extends AppCompatActivity {
     Button loginButton;
+    Button RegisterAsSchoolbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_chooosing);
         loginButton = findViewById(R.id.loginbtn_choosing);
-
+        RegisterAsSchoolbtn=findViewById(R.id.RegisterAschoolbtn);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginChoosingActivity.this, LoginPageActivity.class);
                 startActivity(intent);
 
+
+            }
+
+        });
+        RegisterAsSchoolbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent1 = new Intent(LoginChoosingActivity.this, RegisterAsSchoolActivity.class);
+                startActivity(intent1);
+
+
             }
         });
+
     }
 }
