154721a10fe4ba3162e96b40475fe760939b6548
joxy250 <71070712+joxy250@users.noreply.github.com>
2021-11-05 10:29:13 +0200

    some changes

--- app/src/main/java/com/example/isthateasy2/RegisterAsSchoolActivity.java
+++ app/src/main/java/com/example/isthateasy2/RegisterAsSchoolActivity.java
@@ -3,6 +3,9 @@ package com.example.isthateasy2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterAsSchoolActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RegisterAsSchoolActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_as_school);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = findViewById(R.id.textView12);
+                String name = textView.getText().toString();
+            }
+        });
     }
 }
